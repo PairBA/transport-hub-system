2723a07df883b27c311dcb044c5e83bf0b7e74a2
YuRonan <ronan.han@paircity.com>
2019-07-22 18:21:25 +0800

    ME-1 fix watchDuty

--- src/pages/watchDutySchedule/watchDutySchedule/WatchDutyScheduleHomePage.vue
+++ src/pages/watchDutySchedule/watchDutySchedule/WatchDutyScheduleHomePage.vue
@@ -3,39 +3,40 @@
       <Row>
         <Col span="18" class="table">
           <DatePicker v-model="date" type="month" style="width: 200px"></DatePicker>
-          <Button @click="goSearch">查询</Button>
-          <Row>
-            <Col v-for="day in weekList" :key="day">
-              {{day}}
-            </Col>
-          </Row>
-          <Row>
+          <Button @click="goSearch" style="margin-left: 24px">查询</Button>
+          <Row :gutter="8" style="margin-top: 24px">
             <Col v-for="(singleDay, index) in monthDayList" :key="index" @click.native="highlightDay(singleDay)">
-              <div v-if="singleDay.scheduleDetailList">
-                <div v-for="(oneItem, indexForOne) in singleDay.scheduleDetailList" :key="`singleDay_${indexForOne}`">
-                  <div>{{oneItem.scheduleName}}</div>
-                  <div>
-                    <div v-for="worker in oneItem.scheduleWorkerList" :key="worker.fullName">
-                      {{worker.fullName}}{{worker.mobile}}
+              <div class="item">
+                <div class="calender-num">
+                  {{singleDay.scheduleDate.substring(8)}}
+                </div>
+                <div style="padding: 8px" >
+                  <div v-if="singleDay.scheduleDetailList && singleDay.scheduleDetailList.length">
+                    <div v-for="(oneItem, indexForOne) in singleDay.scheduleDetailList" :key="`singleDay_${indexForOne}`">
+                      <div style="font-size: 12px">{{oneItem.scheduleName}}</div>
+                      <span style="font-size: 14px; font-weight: 600" v-for="worker in oneItem.scheduleWorkerList" :key="worker.fullName">
+                      {{worker.fullName}}
+                    </span>
                     </div>
                   </div>
+                  <div v-else>
+                    暂无排班
+                  </div>
                 </div>
-              </div>
-              <div v-else>
-                {{singleDay}}
               </div>
             </Col>
           </Row>
         </Col>
-        <Col span="6">
-          <div>{{item.scheduleDate}}</div>
+        <Col span="6" class="single-item content__card">
+          <div style="color:rgba(108,117,125,1);font-size:16px;">{{item.scheduleDate}}</div>
+          <Divider/>
           <div v-for="detail in item.scheduleDetailList" :key="detail.scheduleName">
-            {{detail.scheduleName}}
-            <div v-for="worker in detail.scheduleWorkerList" :key="worker.fullName">
-              {{worker.fullName}}{{worker.mobile}}<Icon type="ios-close-circle-outline" @click="deletePlanWorker(detail.scheduleName, worker.fullName, worker.mobile)"/>
+            <span style="font-size:14px;color:rgba(55,66,84,1);font-weight: 500">{{detail.scheduleName}}</span>
+            <div v-for="worker in detail.scheduleWorkerList" :key="worker.fullName" class="worker">
+              {{worker.fullName}}{{worker.mobile}}<Icon type="ios-close-circle-outline" class="icon" @click="deletePlanWorker(detail.scheduleName, worker.fullName, worker.mobile)"/>
             </div>
             <Poptip placement="bottom">
-              <Button>新增值班人员</Button>
+              <Button icon="md-add" long style="margin-top: 12px;margin-bottom: 12px">新增值班人员</Button>
               <div slot="content">
                 <Select v-model="fullName"
                         placeholder="请选择人员">
@@ -96,21 +97,30 @@ export default {
       this.maxDay = date.getDate()
       const monthDayList = []
       date.setDate(1)
-      const week = date.getDay()
-      console.log(week)
+      // const week = date.getDay()
       for (let i = 1; i <= this.maxDay; i++) {
-        monthDayList.push(i)
+        let scheduleDate = dateFormat(this.date, 'yyyy-MM')
+        if (i < 10) {
+          scheduleDate = scheduleDate + '-0' + i
+        } else scheduleDate = scheduleDate + '-' + i
+        monthDayList.push({
+          scheduleDate,
+          scheduleDetailList: []
+        })
       }
       this.planDetailList.forEach(item => {
         const dateStr = item.scheduleDate
         const day = dateStr.substring(8)
         monthDayList[day - 1] = item
       })
-      if (week !== 7) {
-        for (let i = 1; i <= week; i++) {
-          monthDayList.unshift('.')
-        }
-      }
+      // if (week !== 7) {
+      //   for (let i = 1; i <= week; i++) {
+      //     monthDayList.unshift({
+      //       scheduleDate: '.'
+      //     })
+      //   }
+      // }
+      console.log(monthDayList)
       this.monthDayList = monthDayList
     },
     highlightDay(item) {
@@ -160,6 +170,7 @@ export default {
 
 <style lang="less">
 .watchDutySchedule__homePage {
+  padding: 24px;
   .table {
     .ivu-col {
       width: 14.2857%;
@@ -167,6 +178,53 @@ export default {
       -webkit-box-flex: 0;
       -ms-flex: 0 0 auto;
       flex: 0 0 auto;
+    }
+  }
+  .item {
+    width: 92px;
+    height: 140px;
+    background-color: white;
+    margin-bottom: 12px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+  .calender-num {
+    font-size:30px;
+    font-family:DIN-Bold;
+    font-weight:bold;
+    color:rgba(255,255,255,1);
+    line-height:36px;
+    text-align: right;
+    background-image: url("../../../img/calendar.png");
+    background-repeat: no-repeat;
+  }
+  .single-item {
+    padding: 20px 16px;
+    .ivu-divider-horizontal{
+      margin: 16px 0;
+    }
+    .icon {
+      color: #AAB5C8;
+      float: right;
+      font-size: 16px;
+      cursor: pointer;
+    }
+  }
+  .worker {
+    background:rgba(245,248,255,1);
+    border-radius:2px;
+    padding: 6px 14px;
+    font-size:14px;
+    font-family:PingFangSC-Medium;
+    font-weight:500;
+    color:rgba(102,117,141,1);
+    margin-top: 12px;
+  }
+  .ivu-poptip {
+    width: 100%;
+    .ivu-poptip-rel {
+      width: 100%;
     }
   }
 }
