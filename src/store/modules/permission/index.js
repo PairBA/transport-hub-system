62590151267d367a837ed0a1cfdbb4728a22aeee
YuRonan <ronan.han@paircity.com>
2019-08-08 14:55:53 +0800

    ME-2469 fix permission

--- src/store/modules/permission/index.js
+++ src/store/modules/permission/index.js
@@ -43,7 +43,7 @@ const mutations = {
   updateResourceList(state, menuList) {
     // 已经获取到权限菜单
     state.hasGetRules = true
-    state.focusRule = menuList.some(menu => menu.permission === 'vehicleFocusManage')
+    state.focusRule = menuList.some(menu => menu.resourceKey === 'vehicleFocusManage')
     const mainMenu = menuList.filter(menu => menu.resourceKey)
     state.mainMenu = mainMenu
     const firstMenu = mainMenu[0].resourceKey
